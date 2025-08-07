505d5a33debbca14a11ab9371b91b058cd280d07
non4me <vladimir.troyanenko@gmail.com>
2025-08-07 23:26:14 +0200

    Add modal-based emoji picker and refine chat UI.
    - Replace inline emoji picker toggle with modal dialog.
    - Update chat input and message tracking for enhanced usability.
    - Add translations for emoji picker header.
    - Improve router outlet styling for consistent layout.

--- frontend/src/app/chat/chat.component.ts
+++ frontend/src/app/chat/chat.component.ts
@@ -1,20 +1,21 @@
-import {Component, CUSTOM_ELEMENTS_SCHEMA, inject, signal} from '@angular/core';
+import {ChangeDetectorRef, Component, CUSTOM_ELEMENTS_SCHEMA, inject, signal} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {CommonModule} from '@angular/common';
 import {MessageModule} from 'primeng/message';
 import {InputTextModule} from 'primeng/inputtext';
 import {ButtonModule} from 'primeng/button';
 import {SelectModule} from 'primeng/select';
-import 'emoji-picker-element';
 import {TranslateModule} from '@ngx-translate/core';
+import 'emoji-picker-element';
 import {SocketServices} from '../socket.services';
 import {AuthService} from '../auth.service';
+import {Dialog} from 'primeng/dialog';
 
 @Component({
   selector: 'app-chat',
   standalone: true,
   schemas: [CUSTOM_ELEMENTS_SCHEMA],
-  imports: [TranslateModule, CommonModule, FormsModule, MessageModule, InputTextModule, ButtonModule, SelectModule],
+  imports: [TranslateModule, CommonModule, FormsModule, MessageModule, InputTextModule, ButtonModule, SelectModule, Dialog],
   templateUrl: './chat.component.html',
   styleUrl: 'chat.component.scss'
 })
@@ -22,16 +23,24 @@ export class ChatComponent {
   messages;
   newMessage = signal('');
   showEmojiPicker = signal(false);
+  isModalVisible = signal(false);
 
   public authService = inject(AuthService);
   private socketService = inject(SocketServices);
+  private cdr = inject(ChangeDetectorRef);
 
   constructor() {
     this.messages = this.socketService.getMessages();
   }
 
-  toggleEmojiPicker() {
-    this.showEmojiPicker.set(!this.showEmojiPicker());
+  showDialog() {
+    this.isModalVisible.set(true);
+    // this.cdr.markForCheck();
+  }
+
+  onModalClose() {
+    this.isModalVisible.set(false);
+    // this.cdr.markForCheck();
   }
 
   addEmoji(event: any) {
