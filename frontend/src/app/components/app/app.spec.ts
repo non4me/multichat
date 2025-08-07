72c7b2b3a99c7d30b0f907ec8cf876b636bfb72e
non4me <vladimir.troyanenko@gmail.com>
2025-08-08 01:52:31 +0200

    Add unit tests for `AppComponent`
    - Set up `app.spec.ts` with essential dependencies and mock services.
    - Verify component creation and `AuthService` injection.

---
+++ frontend/src/app/components/app/app.spec.ts
@@ -0,0 +1,46 @@
+import {TestBed} from '@angular/core/testing';
+import {AppComponent} from './app.component';
+import {AuthService} from '../../services/auth.service';
+import {TranslateModule, TranslateService} from '@ngx-translate/core';
+import {RouterOutlet} from '@angular/router';
+import {CommonModule} from '@angular/common';
+import {FormsModule} from '@angular/forms';
+import {ButtonModule} from 'primeng/button';
+import {LanguageSelector} from '../language-selector/language-selector';
+import {HttpClientTestingModule} from '@angular/common/http/testing';
+
+describe('AppComponent', () => {
+  let mockAuthService: Partial<AuthService>;
+
+  beforeEach(async () => {
+    mockAuthService = {};
+    await TestBed.configureTestingModule({
+      imports: [
+        TranslateModule,
+        RouterOutlet,
+        CommonModule,
+        FormsModule,
+        ButtonModule,
+        LanguageSelector,
+        HttpClientTestingModule
+      ],
+      providers: [
+        {provide: TranslateService, useValue: {get: () => 'English'}},
+        {provide: AuthService, useValue: mockAuthService}
+      ]
+    }).compileComponents();
+  })
+    ;
+
+    it('should create the AppComponent instance', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      expect(app).toBeTruthy();
+    });
+
+    it('should have authService injected', () => {
+      const fixture = TestBed.createComponent(AppComponent);
+      const app = fixture.componentInstance;
+      expect(app.authService).toBeDefined();
+    });
+  });
